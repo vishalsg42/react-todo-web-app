6bd0b5961c457d95ca4e58a5fe9346f003d8f610
guptavishals <vishalsg@rpdxn.com>
2018-07-08 12:39:28 +0530

    renaming components

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './ToDo.css';
 
-class App extends Component {
+class ToDo extends Component {
   render() {
     return (
       <div className="App">
@@ -18,4 +18,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default ToDo;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Todo from './ToDo';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Todo />, document.getElementById('root'));
 registerServiceWorker();
